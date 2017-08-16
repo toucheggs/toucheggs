2d918199d80cb1d93bdb05c6bee7c5b341b4466b
JJ MG <toucheggs@gmail.com>
2017-08-16 20:06:58 -0300

    bot

--- subir_foto/subir_foto_reacc_o_invol.php
+++ subir_foto/subir_foto_reacc_o_invol.php
@@ -22,7 +22,7 @@ use Facebook\FacebookRequestException;
 FacebookSession::setDefaultApplication('563386293744433', 'b7d60ed8af0cf894f82f56c4c5fa3ec9');
 
 // If you already have a valid access token:
-$session = new FacebookSession('EAAEhpSYPaYUBAK2e391tp8Oi9ZC8LFTkQVkN2WSmj58yZA7p1awNILxZBozW43g74ZBU2jd1Oq69zHkQqqjkKKUS5j9nlTTmLlJgJJhOShcufLG7GUe9BZAAzuACgZBArr6CsPET1F5fq4mYyuZC9zqx5zZB3LA5RPYZD');
+$session = new FacebookSession('EAAEhpSYPaYUBAF1Psy7pumoE4OG2yBiZCp9GRiP6ZAeNx99JRrUcmf6SZB19tawZAuFSNnMLWVrxU4OHAJhYrQvqOECZB4bWX6vRZBiqZBKrjLhmNg8KvZBDQvbJ4H51xzWfb8z9U4ksf2va2ZCSjnEe0OIaV4XqE7qRJQWeKATXekAZDZD');
 
 
 $file = "listado.xml";   // normalmente el xml en wordpress esta en tu dominio.com/feed/
